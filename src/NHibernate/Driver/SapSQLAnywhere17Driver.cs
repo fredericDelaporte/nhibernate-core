6aef60905bd3d613707025745a7fffe7ea9d4ee1
Frédéric Delaporte <fredericdelaporte@free.fr>
2017-07-03 17:04:56 +0200

    Quick&Dirty driver for Anywhere 17.

---
+++ src/NHibernate/Driver/SapSQLAnywhere17Driver.cs
@@ -0,0 +1,27 @@
+﻿namespace NHibernate.Driver
+{
+	/// <summary>
+	/// The SybaseSQLAnywhereDriver Driver provides a database driver for Sap SQL Anywhere 17 and above
+	/// </summary>
+	public class SapSQLAnywhere17Driver : ReflectionBasedDriver
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SapSQLAnywhere17Driver"/> class.
+		/// </summary>
+		/// <exception cref="HibernateException">
+		/// Thrown when the Sap.Data.SQLAnywhere assembly is not found or can not be loaded.
+		/// </exception>
+		public SapSQLAnywhere17Driver()
+			: base("Sap.Data.SQLAnywhere.v4.5", "Sap.Data.SQLAnywhere.SAConnection", "Sap.Data.SQLAnywhere.SACommand")
+		{
+		}
+
+		public override bool UseNamedPrefixInSql => true;
+
+		public override bool UseNamedPrefixInParameter => true;
+
+		public override string NamedPrefix => ":";
+
+		public override bool RequiresTimeSpanForTime => true;
+	}
+}
