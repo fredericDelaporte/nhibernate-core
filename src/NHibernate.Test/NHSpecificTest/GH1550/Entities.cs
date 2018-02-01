0958062d6f0cb23a1ce7640f833eaebaed38a9b4
Frédéric Delaporte <fredericdelaporte@free.fr>
2018-02-01 13:21:06 +0100

    Abstract base class with non-component id test: not queryable.

---
+++ src/NHibernate.Test/NHSpecificTest/GH1550/Entities.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.GH1550
+{
+	public abstract class AbstractClass : IEquatable<AbstractClass>
+	{
+		public virtual KeyClass IdOne { get; set; }
+		public virtual KeyClass IdTwo { get; set; }
+
+		public virtual bool Equals(AbstractClass other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return Equals(IdOne, other.IdOne)
+				&& Equals(IdTwo, other.IdTwo);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != GetType()) return false;
+			return Equals((AbstractClass) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return ((IdOne != null ? IdOne.GetHashCode() : 0) * 397) ^ (IdTwo != null ? IdTwo.GetHashCode() : 0);
+			}
+		}
+
+		public static bool operator ==(AbstractClass left, AbstractClass right) => Equals(left, right);
+
+		public static bool operator !=(AbstractClass left, AbstractClass right) => !Equals(left, right);
+	}
+
+	public class DerivativeOne : AbstractClass
+	{ }
+
+	public class DerivativeTwo : AbstractClass
+	{ }
+
+	public class KeyClass
+	{
+		public virtual Guid Id { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/GH1550/Fixture.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.GH1550
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		private KeyClass _idOne;
+		private KeyClass _idTwo;
+
+		protected override void OnSetUp()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				_idOne = new KeyClass();
+				_idTwo = new KeyClass();
+
+				session.Save(_idOne);
+				session.Save(_idTwo);
+
+				session.Save(
+					new DerivativeOne
+					{
+						IdOne = _idOne,
+						IdTwo = _idTwo
+					});
+
+				transaction.Commit();
+			}
+		}
+
+		protected override void OnTearDown()
+		{
+			using (var session = OpenSession())
+			using (var transaction = session.BeginTransaction())
+			{
+				session.CreateQuery("delete from System.Object").ExecuteUpdate();
+
+				transaction.Commit();
+			}
+		}
+
+		[Test]
+		public void Criteria()
+		{
+			using (var session = OpenSession())
+			using (var tx = session.BeginTransaction())
+			{
+				Assert.That(session.CreateCriteria<AbstractClass>().UniqueResult<AbstractClass>, Throws.Nothing);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void Linq()
+		{
+			using (var session = OpenSession())
+			using (var tx = session.BeginTransaction())
+			{
+				Assert.That(session.Query<AbstractClass>().Single, Throws.Nothing);
+				tx.Commit();
+			}
+		}
+
+		[Test]
+		public void QueryOver()
+		{
+			using (var session = OpenSession())
+			using (var tx = session.BeginTransaction())
+			{
+				Assert.That(session.QueryOver<AbstractClass>().SingleOrDefault, Throws.Nothing);
+				tx.Commit();
+			}
+		}
+	}
+}
