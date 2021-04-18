405ca126ee449100d76af9b78959d859e770b347
Frédéric Delaporte <12201973+fredericdelaporte@users.noreply.github.com>
2021-04-18 19:05:20 +0200

    Add tests for circular references through collections

--- src/NHibernate.Test/NHSpecificTest/GH2563/ByCodeFixture.cs
+++ src/NHibernate.Test/NHSpecificTest/GH2563/ByCodeFixture.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -20,6 +21,34 @@ namespace NHibernate.Test.NHSpecificTest.GH2563
 					rc.Property(x => x.Name, m => m.Lazy(true));
 					rc.ManyToOne(x => x.Child);
 					rc.ManyToOne(x => x.Parent);
+					rc.Set(
+						e => e.Children,
+						m =>
+						{
+							m.Key(c => c.Column("OtherParent"));
+							m.Inverse(true);
+						},
+						r => r.OneToMany());
+					rc.ManyToOne(x => x.OtherParent);
+					rc.Set(
+						e => e.Related,
+						m =>
+						{
+							m.Table("Related");
+							m.Key(c => c.Column("Related"));
+						},
+						r => r.ManyToMany(
+							m => m.Column("InverseRelated")));
+					rc.Set(
+						e => e.InverseRelated,
+						m =>
+						{
+							m.Table("Related");
+							m.Key(c => c.Column("InverseRelated"));
+							m.Inverse((true));
+						},
+						r => r.ManyToMany(
+							m => m.Column("Related")));
 				});
 
 			return mapper.CompileMappingForAllExplicitlyAddedEntities();
@@ -30,11 +59,32 @@ namespace NHibernate.Test.NHSpecificTest.GH2563
 			using (var session = OpenSession())
 			using (var transaction = session.BeginTransaction())
 			{
-				var e1 = new Entity { Id = 1, Name = "Middle", };
+				var e1 = new Entity { Id = 1, Name = "Child", };
 				e1.Parent = new Entity { Id = 2, Name = "Parent", Child = e1 };
-
 				session.Save(e1);
 				session.Save(e1.Parent);
+
+				var e3 = new Entity
+				{
+					Id = 3,
+					Name = "Child",
+					OtherParent = new Entity { Id = 4, Name = "OtherParent" },
+				};
+				session.Save(e3);
+				session.Save(e3.OtherParent);
+
+				var e5 = new Entity { Id = 5, Name = "Related", Related = new HashSet<Entity>()};
+				var e6 = new Entity { Id = 6, Name = "InverseRelated"};
+				e5.Related.Add(e6);
+				session.Save(e5);
+				session.Save(e6);
+
+				var e7 = new Entity { Id = 7, Name = "CrossRelated1", Related = new HashSet<Entity>()};
+				var e8 = new Entity { Id = 8, Name = "CrossRelated2", Related = new HashSet<Entity>()};
+				e7.Related.Add(e8);
+				e8.Related.Add(e7);
+				session.Save(e7);
+				session.Save(e8);
 
 				transaction.Commit();
 			}
@@ -45,7 +95,7 @@ namespace NHibernate.Test.NHSpecificTest.GH2563
 			using (var session = OpenSession())
 			using (var transaction = session.BeginTransaction())
 			{
-				session.CreateQuery("delete from System.Object").ExecuteUpdate();
+				session.Delete("from System.Object");
 				transaction.Commit();
 			}
 		}
@@ -55,8 +105,58 @@ namespace NHibernate.Test.NHSpecificTest.GH2563
 		{
 			using (var session = OpenSession())
 			{
-				var result = session.Query<Entity>().ToList();
-				var ds = SpoofSerialization(session);
+				var result = session.Query<Entity>().Where(e => e.Id < 3).ToList();
+				var e1 = result.FirstOrDefault((e => e.Id == 1));
+				Assert.That(e1, Is.Not.Null);
+				Assert.That(e1.Parent, Is.Not.Null);
+				Assert.That(e1, Is.EqualTo(e1.Parent.Child));
+				SpoofSerialization(session);
+			}
+		}
+
+		[Test]
+		public void SerializationOfCollectionConnectedEntitiesWithLazyProperties()
+		{
+			using (var session = OpenSession())
+			{
+				var result = session.Query<Entity>().Where(e => e.Id > 2 && e.Id < 5).ToList();
+				var e3 = result.FirstOrDefault((e => e.Id == 3));
+				Assert.That(e3, Is.Not.Null);
+				Assert.That(e3.OtherParent, Is.Not.Null);
+				Assert.That(e3.OtherParent.Children, Contains.Item(e3));
+				SpoofSerialization(session);
+			}
+		}
+
+		[Test]
+		public void SerializationOfManyToManyConnectedEntitiesWithLazyProperties()
+		{
+			using (var session = OpenSession())
+			{
+				var result = session.Query<Entity>().Where(e => e.Id > 4 && e.Id < 7).ToList();
+				var e5 = result.FirstOrDefault((e => e.Id == 5));
+				var e6 = result.FirstOrDefault((e => e.Id == 6));
+				Assert.That(e5, Is.Not.Null);
+				Assert.That(e5.Related, Contains.Item(e6));
+				Assert.That(e6, Is.Not.Null);
+				Assert.That(e6.InverseRelated, Contains.Item(e5));
+				SpoofSerialization(session);
+			}
+		}
+
+		[Test]
+		public void SerializationOfCrossManyToManyConnectedEntitiesWithLazyProperties()
+		{
+			using (var session = OpenSession())
+			{
+				var result = session.Query<Entity>().Where(e => e.Id > 6).ToList();
+				var e7 = result.FirstOrDefault((e => e.Id == 7));
+				var e8 = result.FirstOrDefault((e => e.Id == 8));
+				Assert.That(e7, Is.Not.Null);
+				Assert.That(e7.Related, Contains.Item(e8));
+				Assert.That(e8, Is.Not.Null);
+				Assert.That(e8.Related, Contains.Item(e7));
+				SpoofSerialization(session);
 			}
 		}
 
--- src/NHibernate.Test/NHSpecificTest/GH2563/Entity.cs
+++ src/NHibernate.Test/NHSpecificTest/GH2563/Entity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace NHibernate.Test.NHSpecificTest.GH2563
 {
@@ -8,6 +9,10 @@ namespace NHibernate.Test.NHSpecificTest.GH2563
 		public virtual int Id { get; set; }
 		public virtual Entity Parent { get; set; }
 		public virtual Entity Child { get; set; }
+		public virtual Entity OtherParent { get; set; }
+		public virtual ISet<Entity> Children { get; set; }
+		public virtual ISet<Entity> Related { get; set; }
+		public virtual ISet<Entity> InverseRelated { get; set; }
 		public virtual string Name { get; set; }
 	}
 }
